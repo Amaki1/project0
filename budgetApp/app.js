16429de71fbc79ed9eef6f771002de70e635d839
amaki <amal.ourdou@gmail.com>
2023-04-25 21:25:20 +0100

    js tuto

---
+++ budgetApp/app.js
@@ -0,0 +1,15 @@
+// variables (var, let, const)
+
+// data types and structures (string, numbers, booleans, arrays, objects)
+
+// functions (traditional functions vs arrow functions () => {})
+
+// interacting with HTML & CSS (DOM manipulation)
+
+//scoping and hoisting
+
+// string concatenation (+) vs template literals (``)
+
+
+
+
